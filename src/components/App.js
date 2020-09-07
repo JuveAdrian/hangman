206e737ca5e668a0b056563966ac2be15fb6b4fa
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-09-08 01:19:35 +0200

    simple routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Hangman from '../pages/hangman';
+
 class App extends React.Component {
     render () {
         return (
-            <div>
-                <h1>Test</h1>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Hangman} />
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ src/pages/hangman.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hangman extends React.Component {
+    render() {
+        return(
+            <div>
+                Hangman
+            </div>
+        )
+    }
+}
+
+export default Hangman;
