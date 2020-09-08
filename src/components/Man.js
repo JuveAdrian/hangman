6df6403e5c8bfa16af05592bc0c30adac48ee534
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-09-08 02:59:34 +0200

    save

---
+++ src/components/Man.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Man extends React.Component {
+    render () {
+        return (
+            <div>
+                Man
+            </div>
+        )
+    }
+}
+
+export default Man;
